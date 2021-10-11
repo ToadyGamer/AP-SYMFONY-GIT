1bb56090b0755e82b10b32db0547fb4555d9bc3a
Ruby <rubybutventing@gmail.com>
2021-10-11 15:48:33 +0200

    <Feat> Page d'accueil & Page d'avis

--- APSymfonyFlorianEloiRuby/src/Controller/Controller.php
+++ APSymfonyFlorianEloiRuby/src/Controller/Controller.php
@@ -4,7 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Exemple;
+use App\Entity\Avis;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Form;
 
 class Controller extends AbstractController
 {
@@ -15,6 +24,62 @@ class Controller extends AbstractController
     {
         return $this->render('/index.html.twig', [
             'controller_name' => 'Controller',
+        ]);
+    }
+
+    /**
+     * @Route("/presentation", name="presentation")
+     */
+    public function presentation(): Response
+    {
+        return $this->render('/presentation.html.twig', [
+            'controller_name' => 'Controller',
+        ]);
+    }
+        /**
+     * @Route("/exemple", name="exemple")
+     */
+    public function exemple(): Response
+    {
+        $Data = $this->getDoctrine()->getRepository(Exemple::class)->findAll();
+        return $this->render('/exemple.html.twig', [
+            'controller_name' => 'Controller',
+            'Data'=>$Data,
+        ]);
+    }
+    /**
+     * @Route("/avis", name="avis")
+     */
+    public function avis(Request $request): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Avis::class);
+        $Data2 = $repo->findAll();
+        $avis = new Avis();
+        $form = $this->createFormBuilder($avis)
+            ->add('pseudoClientAvis',TextType::class, array('label'=> 'Nom'))
+            ->add('nombreEtoilesAvis',TextType::class, array('label'=> 'Note'))
+            ->add('commentaireAvis',TextareaType::class, array('label'=> 'Commentaire'))
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        if ($request->isMethod('POST')){
+
+            $form->submit($request->request->get($form->getName()));
+            if($form->isSubmitted() && $form->isValid()){
+                //...more stuff pre-insertion here if needed
+                $em = $this->getDoctrine()->getManager();
+                $avis=$form->getData();
+                $em->persist($avis);//persist the contact object
+                $em->flush();//save it to the db
+                //...more stuff post-insertion here if needed
+                return $this->redirectToRoute('avis');
+            }
+        }
+
+        return $this->render('/avis.html.twig', [
+            'controller_name' => 'Controller',
+            'Data2'=>$Data2,
+            'form'=>$form->createView(),
         ]);
     }
 }
---
+++ APSymfonyFlorianEloiRuby/src/Entity/Exemple.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExempleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ExempleRepository::class)
+ */
+class Exemple
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomProjet;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Adresse;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cp;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $imageExemple;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Commentaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomProjet(): ?string
+    {
+        return $this->nomProjet;
+    }
+
+    public function setNomProjet(string $nomProjet): self
+    {
+        $this->nomProjet = $nomProjet;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getCp(): ?int
+    {
+        return $this->cp;
+    }
+
+    public function setCp(int $cp): self
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getImageExemple(): ?string
+    {
+        return $this->imageExemple;
+    }
+
+    public function setImageExemple(?string $imageExemple): self
+    {
+        $this->imageExemple = $imageExemple;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->Commentaire;
+    }
+
+    public function setCommentaire(string $Commentaire): self
+    {
+        $this->Commentaire = $Commentaire;
+
+        return $this;
+    }
+}
